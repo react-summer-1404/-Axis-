ebe5d02af41c2e66bcef4392f7b59896c0593662
farniyafooladi-dotcom <farniyafooladi@gmail.com>
2025-10-25 13:50:04 +0330

    feat: Add dark Navbar

--- Axis/src/Common/Navbar.jsx
+++ Axis/src/Common/Navbar.jsx
@@ -8,12 +8,12 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='h-[120px] border m-auto relative ' >
+    <div className='h-[120px] border m-auto relative' >
         {/* logo */}
         <Link to="/">
           <div  className='flex items-center flex-row-reverse gap-3 justify-end px-3 mt-7' dir='rtl'   > 
-              <div className='text-right' > <p className='text-lg font-bold text-gray-800'>نامبر وان</p> 
-              <p className='text-sm text-gray-600'> همیشه بهترین</p>
+              <div className='text-right' > <p className='text-lg font-bold text-gray-800 dark:text-gray-100'>نامبر وان</p> 
+              <p className='text-sm text-gray-600 dark:text-gray-100'> همیشه بهترین</p>
               </div>
               <div className='w-12 h-12 to-[#7572AB] from-[#5751E1] bg-gradient-to-br text-white rounded-full shadow-md flex items-center justify-center '>
                   <span className='text-lg font-bold'>N1</span>
@@ -22,27 +22,27 @@ const Navbar = () => {
         </Link>
         {/* menu */}
         <div className=' flex flex-row gap-4 px-52 relative bottom-8 ' dir='rtl'> 
-        <p className='hover:text-[#5751E1] cursor-pointer'>
+        <p className='hover:text-[#5751E1] cursor-pointer dark:text-gray-300 dark:hover:text-[#5751E1]'>
           <Link to='/'>صفحه اصلی</Link></p>    
-        <p className='hover:text-[#5751E1]  cursor-pointer'><Link to="/courses"> دوره ها</Link></p>
-        <p className='hover:text-[#5751E1]  cursor-pointer'>ایونت ها</p>
-        <p className='hover:text-[#5751E1]  cursor-pointer'> فروشگاه</p>
-        <p className='hover:text-[#5751E1]  cursor-pointer'>وبلاگ </p>
-        <p className='hover:text-[#5751E1]  cursor-pointer'>مربیان</p>
+        <p className='hover:text-[#5751E1]  cursor-pointer dark:text-gray-300 dark:hover:text-[#5751E1]'><Link to="/courses"> دوره ها</Link></p>
+        <p className='hover:text-[#5751E1]  cursor-pointer dark:text-gray-300 dark:hover:text-[#5751E1]'>ایونت ها</p>
+        <p className='hover:text-[#5751E1]  cursor-pointer dark:text-gray-300 dark:hover:text-[#5751E1]'> فروشگاه</p>
+        <p className='hover:text-[#5751E1]  cursor-pointer dark:text-gray-300 dark:hover:text-[#5751E1]'>وبلاگ </p>
+        <p className='hover:text-[#5751E1]  cursor-pointer dark:text-gray-300 dark:hover:text-[#5751E1]'>مربیان</p>
         </div>
         {/* search */}
-        <div className='border rounded-full w-[400px] h-[50px] border-gray-300 absolute bottom-9 left-[320px] flex '>
+        <div className='border rounded-full w-[400px] h-[50px] border-gray-300 absolute bottom-9 left-[320px] flex dark:bg-slate-100'>
           <div> 
-          <select className="  px-1 py-1 text-right focus:outline-none  relative top-2 left-60 bg-white " dir='rtl'>
+          <select className="  px-1 py-1 text-right focus:outline-none  relative top-2 left-60 bg-white dark:bg-slate-100" dir='rtl'>
           <option>دسته‌بندی‌ها</option>
            <option>برنامه‌نویسی</option>
            <option>طراحی</option>
            </select>
           </div>
    
-          <img src={ImgIcon} className=' absolute bottom-4 right-4' />
+          <img src={ImgIcon} className=' absolute bottom-4 right-4 ' />
           <img src={Button} className='size-11 absolute  left-2 top-[2px]'/>
-          <input type='search'  placeholder='جستجو برای دوره...' dir='rtl' className=' relative right-1 w-32 focus:outline-none'/>
+          <input type='search'  placeholder='جستجو برای دوره...' dir='rtl' className=' relative right-1 w-32 focus:outline-none dark:bg-slate-100'/>
          </div>
        <img src={Header} className='size-10 absolute top-9 left-[260px]'/> 
        <img src={Like} className='size-10 absolute top-9 left-[210px]' />
