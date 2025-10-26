8e63a4fd7101a474366281fd5a18fde502fac716
farniyafooladi-dotcom <farniyafooladi@gmail.com>
2025-10-27 02:37:40 +0330

    feat: Header Courses

---
+++ Axis/src/Pages/CoursesDetails/HeaderDetails.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HeaderDetails = () => {
+  return (
+    <div className='border w-[1440] h-[100px] bg-[#EFEBF9] flex justify-center  items-center  flex-col gap-2 cursor-pointer'>
+    <span className='text-2xl font-bold '>حل تعارضات  بین طراحان و مهندسان</span>
+
+    <div className='flex gap-3 text-sm ' dir='rtl'> 
+    <span> صفحه اصلی </span> 
+    <span>دوره های آموزشی</span> 
+    <span className='text-[#5751E1]'> حل تعارضات بین طراحان و مهندسان</span> 
+    </div>
+   
+   </div>
+  )
+}
+
+export default HeaderDetails
