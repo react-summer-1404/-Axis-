893c14223d74269c0d45acb12bcf70a50be6559f
farniyafooladi-dotcom <farniyafooladi@gmail.com>
2025-11-14 15:33:12 +0330

    update section header

--- Axis/src/Pages/Dashboard/SectionHeader.jsx
+++ Axis/src/Pages/Dashboard/SectionHeader.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 const SectionHeader = () => {
   return (
-    <div className="flex justify-between items-center mb-4 mt-6 border-b border-gray-200 pb-2">
-      <a href="#" className="text-sm text-gray-500 hover:text-gray-700 transition-colors">
-        مشاهده همه &rarr;
-      </a>
-      <h2 className="text-xl font-bold text-gray-800">دوره‌های در حال برگزاری</h2>
-      <h2 className="text-xl font-bold text-gray-800"> دوره های پیشنهادی</h2>
+    <div className="flex  justify-start gap-96 items-center mb-4 mt-6  pb-2">
+     
+      <h2 className="text-xl  text-[#777777]">دوره‌های در حال برگزاری :</h2>
+      <h2 className="text-xl text-[#777777] "> دوره های پیشنهادی :</h2>
+     
     </div>
   );
 };
