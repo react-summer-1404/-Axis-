150e3e3f89c42c5d5d6d17caf003a4e0c4b8287a
farniyafooladi-dotcom <farniyafooladi@gmail.com>
2025-11-21 23:03:33 +0330

    Update Courses.js

--- Axis/src/Api/Dashboard/courses.js
+++ Axis/src/Api/Dashboard/courses.js
@@ -1,12 +1,12 @@
-// data/courses.js
+
 export const initialCourses = [
-    { id: 1, name: "آموزش Tailwind CSS", teacher: "دکتر محمدحسین بحرالعلومی", price: "۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
-    { id: 2, name: "آموزش کامل کار با Figma", teacher: "دکتر محمدحسین بحرالعلومی", price: "۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
-    { id: 3, name: "آموزش Next.js", teacher: "محمد رضا ساداتی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️"},
-    { id: 4, name: "آموزش Front-End", teacher: "دکتر محمدحسین بحرالعلومی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️"},
-    { id: 5, name: "آموزش پیشرفته Tailwind CSS", teacher: "دکتر محمدحسین بحرالعلومی", price: "۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
-    { id: 6, name: "آموزش Figma برای UI/UX", teacher: "دکتر محمدحسین بحرالعلومی", price: "۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
-    { id: 7, name: "آموزش Next.js پایه", teacher: "محمد رضا ساداتی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
-    { id: 8, name: "آموزش جامع Front-End", teacher: "دکتر محمدحسین بحرالعلومی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
-    { id: 9, name: "آموزش Next.js پیشرفته", teacher: "محمد رضا ساداتی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️"},
+    { id: 1, name: "Tailwind CSS آموزش ", teacher: "دکتر محمدحسین بحرالعلومی", price: "۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon:  "⚛️" },
+    { id: 2, name: "Figma آموزش کامل کار با ", teacher: "دکتر محمدحسین بحرالعلومی", price: "۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
+    { id: 3, name: "Next.js آموزش ", teacher: "محمد رضا ساداتی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️"},
+    { id: 4, name: "Front-End آموزش ", teacher: "دکتر محمدحسین بحرالعلومی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️"},
+    { id: 5, name: "Tailwind CSS آموزش پیشرفته ", teacher: "دکتر محمدحسین بحرالعلومی", price: "۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
+    { id: 6, name: "Figma برای UI/UX آموزش ", teacher: "دکتر محمدحسین بحرالعلومی", price: "۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
+    { id: 7, name: "Next.js آموزش  پایه", teacher: "محمد رضا ساداتی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
+    { id: 8, name: "Front-End آموزش جامع ", teacher: "دکتر محمدحسین بحرالعلومی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
+    { id: 9, name: "Next.js آموزش پیشرفته", teacher: "محمد رضا ساداتی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️"},
   ];
