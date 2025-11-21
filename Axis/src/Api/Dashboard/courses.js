e97edf50dba2f03203fbd7a89ebb4d7b76114931
farniyafooladi-dotcom <farniyafooladi@gmail.com>
2025-11-21 16:57:18 +0330

    fake api courses

---
+++ Axis/src/Api/Dashboard/courses.js
@@ -0,0 +1,12 @@
+// data/courses.js
+export const initialCourses = [
+    { id: 1, name: "آموزش Tailwind CSS", teacher: "دکتر محمدحسین بحرالعلومی", price: "۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
+    { id: 2, name: "آموزش کامل کار با Figma", teacher: "دکتر محمدحسین بحرالعلومی", price: "۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
+    { id: 3, name: "آموزش Next.js", teacher: "محمد رضا ساداتی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️"},
+    { id: 4, name: "آموزش Front-End", teacher: "دکتر محمدحسین بحرالعلومی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️"},
+    { id: 5, name: "آموزش پیشرفته Tailwind CSS", teacher: "دکتر محمدحسین بحرالعلومی", price: "۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
+    { id: 6, name: "آموزش Figma برای UI/UX", teacher: "دکتر محمدحسین بحرالعلومی", price: "۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
+    { id: 7, name: "آموزش Next.js پایه", teacher: "محمد رضا ساداتی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
+    { id: 8, name: "آموزش جامع Front-End", teacher: "دکتر محمدحسین بحرالعلومی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️" },
+    { id: 9, name: "آموزش Next.js پیشرفته", teacher: "محمد رضا ساداتی", price: "۲,۵۰,۰۰۰", date: "۱۴۰۳/۰۳/۱۸", icon: "⚛️"},
+  ];
