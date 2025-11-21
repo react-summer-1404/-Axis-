d7e1a91d09e68f66cfbc174ab5d6d0b330718cad
farniyafooladi-dotcom <farniyafooladi@gmail.com>
2025-11-21 17:25:19 +0330

    status

---
+++ Axis/src/Pages/CourseList/StatusBadge.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const StatusBadge = ({ status }) => {
+  let classes = 'inline-flex items-center px-3 py-1 text-xs font-semibold rounded-full ';
+  let text = status;
+
+  if (status === "تایید شده") {
+    classes += 'bg-green-100 text-green-800';
+  } else if (status === "در انتظار تایید") {
+    classes += 'bg-orange-100 text-orange-800';
+  } else {
+    classes += 'bg-gray-100 text-gray-800';
+  }
+
+  return (
+    <span className={classes}>
+      {text}
+    </span>
+  );
+};
+
+export default StatusBadge;
